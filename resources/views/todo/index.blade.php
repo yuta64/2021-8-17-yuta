0601ea46fa9e17b9a83b9943c98c05f0e0b3be50
yuta <jampingdolfinyuta27@gmail.com>
2021-08-05 12:47:49 +0900

    commit

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -1,21 +1,17 @@
 <!DOCTYPE html>
-<html lang="jp">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="resources/css/reset.css" />
-     <link rel="stylesheet" href="resources/css/style.css"/>
 </head>
 <body>
-
-@csrf
-<form action="">
-    <input type="text" name="content" value="">
-    <button>追加</button>
-</form>
-
+<h1>Todo</h1>
+<ul>
+    @foreach ($items as $item)
+    <li>{{$item ->content}}</li>
+    @endforeach
+</ul>
 </body>
 </html>
-P
