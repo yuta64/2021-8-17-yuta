7bda9b3ab82a3c62d82da023ea3fb5fdb3d40fa6
yuta <jampingdolfinyuta27@gmail.com>
2021-08-05 11:45:52 +0900

    first-commit

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    public function index()
+    {
+        $items = Todo::all();
+
+
+        return view('todo.index',['items'=>$items]);
+    }
+}
---
+++ resources/views/todo/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="resources/css/reset.css" />
+     <link rel="stylesheet" href="resources/css/style.css"/>
+</head>
+<body>
+
+@csrf
+<form action="">
+    <input type="text" name="content" value="">
+    <button>追加</button>
+</form>
+
+</body>
+</html>
+P
